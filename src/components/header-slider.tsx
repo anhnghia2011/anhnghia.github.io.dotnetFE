4cadc8946ff28d3d0bef5cae564d00973305c55b
nghia.nguyen <nghia.nguyen@s3corp.com.vn>
2024-10-01 17:21:29 +0700

    cm

--- src/components/header-slider.tsx
+++ src/components/header-slider.tsx
@@ -2,11 +2,11 @@
 function HeaderSlider() {
     return (
         <div>
-            <div className="flex flex-col items-center justify-center w-full pt-5">
+            <div className="flex flex-col items-center justify-center w-full pt-5 gap-2">
                 <p className="font-normal font-sans">Nike Running</p>
                 <h1 className="font-bold text-6xl">WINNING ISN’T COMFORTABLE</h1>
                 <p className="font-normal font-sans">If you don't hate running a little, you don't love running enough.</p>
-                <button className="">Shop Running</button>
+                <button className="rounded-xl px-3 py-1 bg-black text-white">Shop Running</button>
             </div>
             <div></div>
         </div>
